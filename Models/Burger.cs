51b01eec4f43188b52f15f7570138b2638292640
Hayland Montalvo <hayland.montalvo@udla.edu.ec>
2023-10-11 08:57:37 -0500

    Agregar archivos de proyecto.

---
+++ Models/Burger.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HaylandMontalvo_ProyectoCF.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }  
+        public bool WithCheese { get; set; }
+        [Range(0.01 , 3.50)]
+        public decimal precio { get; set; } 
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HaylandMontalvo_ProyectoCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
